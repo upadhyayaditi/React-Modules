e40505ce3f02e7a369588d58df58fd06a39816b7
upadhyayaditi <au.aditi08@gmail.com>
2024-02-21 14:15:11 +0530

    context project

---
+++ contextProject/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import UserContext from '../context/UserContext'
+
+
+function Profile() {
+    const {user} = useContext(UserContext)
+
+ 
+    if(!user) return <div>Please login</div>
+    return <div>welcome: {user.username}</div>
+  
+}
+
+export default Profile
