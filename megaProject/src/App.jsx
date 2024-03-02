8e2aecf7d4a08db800b06f084f4e350faf5ff846
upadhyayaditi <au.aditi08@gmail.com>
2024-03-02 09:55:28 +0530

    Configure Redux toolkit

--- megaProject/src/App.jsx
+++ megaProject/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Footer, Header} from './components'
+import {Outlet} from 'react-router-dom'
+
+
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
   
-  return (
-    <>
-      <h1>A blog with Appwrite</h1>
-    </>
-  )
+ useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) =>{
+    if (userData) {
+      dispatch(login({userData}))
+    }else{
+      dispatch(logout())
+    }
+  } )
+  .finally(()=> setLoading(false))
+ },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between '>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+        
+      </div>
+
+    </div>
+  ) : null
+
+
 }
 
 export default App
